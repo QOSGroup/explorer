cb327e0baa4225cf7acae3541efcdc306a883199
zhangwenshun <378253487@qq.com>
2018-10-25 11:13:29 +0800

    优化代码

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  // BASE_API: '"https://easy-mock.com/mock/5ba36527a1924362236a3d8d"',
   BASE_API: '"http://192.168.168.183:8081"',
 })
