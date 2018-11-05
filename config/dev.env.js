42ffb990be24079e8960fcf5ad234fe6b3462b46
文顺 章 <378253487@qq.com>
2018-11-05 15:11:09 +0800

    修改package.json中name

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://192.168.168.183:8081"',
+  BASE_API: '"http://192.168.1.183:8081"',
 })
