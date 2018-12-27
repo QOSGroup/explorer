b2e04bc68a7c20dc2856bd57d9b8a155c704a4ff
xutao <xuyz1988@163.com>
2018-12-27 17:07:09 +0800

    fix modify base api

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://dev-qmoonapi.qoschain.com"',
-  // BASE_API: '"http://qmoonapi.qoschain.info"',
+  BASE_API: '"http://qmoonapi.qoschain.info"',
 })
