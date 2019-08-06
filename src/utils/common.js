fb6b1539e4c1338630eca5b787165df150394714
saber200 <htmlgzzy@126.com>
2019-08-06 11:11:18 +0800

    修改content title样式 增加json格式化方法

--- src/utils/common.js
+++ src/utils/common.js
@@ -25,3 +25,34 @@ export const getNodeDetail = (id) => {
   const filterNodes = allNode.filter(x => x.node_info.id === id)
   return filterNodes.length > 0 ? filterNodes[0] : null
 }
+
+/**
+ * 
+ * @param {格式化json} msg json字符串
+ */
+export const formatJson = msg => {
+  const rep = "~";
+  let jsonStr = JSON.stringify(msg, null, rep), str = "";
+  for (let i = 0; i < jsonStr.length; i++) {
+      let text2 = jsonStr.charAt(i)
+      if (i > 1) {
+          let text = jsonStr.charAt(i - 1)
+          if (rep != text && rep == text2) {
+              str += "<br/>"
+          }
+      }
+      str += text2;
+  }
+  jsonStr = "";
+  for (let i = 0; i < str.length; i++) {
+      let text = str.charAt(i);
+      if (rep == text){
+          jsonStr += "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+      }else {
+          jsonStr += text;
+      }
+      if (i == str.length - 2)
+          jsonStr += "<br/>"
+  }
+  return jsonStr;
+}
